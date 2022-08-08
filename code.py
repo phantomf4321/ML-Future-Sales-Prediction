b07f71bcb1d9942883a67e75e98cd7fa514957e1
Sajjad Ranjbar <83742735+phantomf4321@users.noreply.github.com>
2022-08-09 00:17:45 +0430

    Update code.py

--- code.py
+++ code.py
@@ -46,3 +46,34 @@ figure.show()
 figure = px.scatter(data_frame = data, x="Sales",
                     y="Radio", size="Radio", trendline="ols")
 figure.show()
+
+correlation = data.corr()
+print(correlation["Sales"].sort_values(ascending=False))
+"""
+OUT:
+Sales        1.000000
+TV           0.901208
+Radio        0.349631
+Newspaper    0.157960
+Name: Sales, dtype: float64
+"""
+
+#split the data into training and test sets
+x = np.array(data.drop(["Sales"], 1))
+y = np.array(data["Sales"])
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2,  random_state=42)
+
+#train the model to predict future sales
+model = LinearRegression()
+model.fit(xtrain, ytrain)
+print(model.score(xtest, ytest))
+
+#input values into the model according to the features we have used to train
+#features = [[TV, Radio, Newspaper]]
+features = np.array([[153.1, 307.8, 9.2]])
+print(model.predict(features))
+
+"""
+OUT:
+[44.17037546]
+"""
