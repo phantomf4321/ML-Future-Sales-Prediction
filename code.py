bb380ef47dc8f6b5389a0a2f6749483b2a22da41
Sajjad Ranjbar <83742735+phantomf4321@users.noreply.github.com>
2022-08-09 00:11:38 +0430

    Update code.py

--- code.py
+++ code.py
@@ -8,4 +8,29 @@ from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/advertising.csv")
 #print(data)
+"""
+OUT:
+        TV  Radio  Newspaper  Sales
+0    230.1   37.8       69.2   22.1
+1     44.5   39.3       45.1   10.4
+2     17.2   45.9       69.3   12.0
+3    151.5   41.3       58.5   16.5
+4    180.8   10.8       58.4   17.9
+..     ...    ...        ...    ...
+195   38.2    3.7       13.8    7.6
+196   94.2    4.9        8.1   14.0
+197  177.0    9.3        6.4   14.8
+198  283.6   42.0       66.2   25.5
+199  232.1    8.6        8.7   18.4
+
+[200 rows x 4 columns]
+"""
 #print(data.isnull().sum())
+"""
+OUT:
+TV           0
+Radio        0
+Newspaper    0
+Sales        0
+dtype: int64
+"""
