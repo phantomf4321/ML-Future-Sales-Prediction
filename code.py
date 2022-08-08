c63bff0c66c13c36d0760327c9482bd78944c154
Sajjad Ranjbar <83742735+phantomf4321@users.noreply.github.com>
2022-08-09 00:10:10 +0430

    Update code.py

--- code.py
+++ code.py
@@ -4,3 +4,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+
+
+data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/advertising.csv")
+#print(data)
+#print(data.isnull().sum())
