4157931b60b63a06d83123da001e4ee90fadc90e
Sajjad Ranjbar <83742735+phantomf4321@users.noreply.github.com>
2022-08-09 13:36:03 +0430

    Update code.py

--- code.py
+++ code.py
@@ -5,7 +5,6 @@ import plotly.graph_objects as go
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-
 data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/advertising.csv")
 #print(data)
 """
